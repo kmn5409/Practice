f6b24a176bd77800acf4c1d523b30aae59c0b710
Keanu Nichols <nichols.keanu9@gmail.com>
2019-10-29 14:49:02 -0400

    Update test.py

--- test.py
+++ test.py
@@ -2,17 +2,17 @@ import unittest
 import calculator
 
 class Test(unittest.TestCase):
-  def test_add(self):
-    self.assertEqual(calculator.add(2, 3), 5)
+    def test_add(self):
+        self.assertEqual(calculator.add(2, 3), 5)
     
     def test_sub(self):
-      self.assertEqual(calculator.sub(2, 3), -1)
-      
+        self.assertEqual(calculator.sub(2, 3), -1)
+  
     def test_mult(self):
-      self.assertEqual(calculator.mult(2, 3), 6)
-      
+        self.assertEqual(calculator.mult(2, 3), 6)
+
     def test_div(self):
-      self.assertEqual(calculator.div(6, 3), 2)
-      
-   if __name__ == '__main__':
+        self.assertEqual(calculator.div(6, 3), 2)
+  
+if __name__ == '__main__':
     unittest.main()
